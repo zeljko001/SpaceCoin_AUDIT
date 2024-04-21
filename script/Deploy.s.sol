3432166f391daca578dfb154d7715782dcd70716
nedicp <petar.nedic14@gmail.com>
2024-04-21 19:11:37 +0200

    initial commit1

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,7 +8,7 @@ import {SpaceCoin} from "../src/SpaceCoin.sol";
 contract Deploy is Script {
 
     address treasuryAccount = 0xB3B02E7767bF65d5693a2056C417eC4E3e876Eb7; //1
-    address[] private allowList = [0x30C816eB8F5701b12687269F2601Cb6ff8A20510, 0x87F9139461e3781B011d18094f19Bc3f86876cF3];//2,3
+    address[] private allowList = [0x30C816eB8F5701b12687269F2601Cb6ff8A20510, 0xB9e1ED8Cc46483e9b5901326cbF8381b9b200905];//2,5
     // following 2 lines are used for testing.
     // address treasuryAccount = address(1); //1
     // address[] private allowList = [address(2), address(3)];//2,3
